79f4271a094a45c79c450b8053f34c092de266e6
amihaiemil <amihaiemil@gmail.com>
2020-05-08 17:35:15 +0300

    update articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,16 +11,33 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
   def create
-    @article = Article.new(
-      params
-        .require(:article)
-        .permit(:title, :text)
-    )
+    @article = Article.new(self.article_params())
     if @article.save
       redirect_to @article
     else
       render 'new'
     end
   end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(self.article_params())
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
+  private
+    def article_params
+      params
+        .require(:article)
+        .permit(:title, :text)
+    end
 end
