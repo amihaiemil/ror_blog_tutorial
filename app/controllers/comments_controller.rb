6b78a2287a91c1d83a6ff7fff1f1f6b7da43a78d
amihaiemil <amihaiemil@gmail.com>
2020-05-08 18:27:28 +0300

    Article Comments page

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,7 @@
 class CommentsController < ApplicationController
+  def index
+    @article = Article.find(params[:article_id])
+  end
   def create
     @article = Article.find(params[:article_id])
     @comment = @article.comments.create(comment_params)
